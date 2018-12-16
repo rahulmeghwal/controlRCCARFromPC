d2eb50db18c28ed08031647a9d935c0b458e6cb8
Rahul Meghwal <rahulmeghwal.rm@gmail.com>
2018-12-16 12:18:00 +0530

    Update arduinoSerialRead.ino

--- readKeyboardAndWriteSerial/Arduino/arduinoSerialRead/arduinoSerialRead.ino
+++ readKeyboardAndWriteSerial/Arduino/arduinoSerialRead/arduinoSerialRead.ino
@@ -1,13 +1,20 @@
 char data = '0';
 int val = -1;
 
-// Car movements 
+// Car movements signals from arduino
 int left = 0;
 int right = 0;
 int forward = 0;
 int reverse = 0;
 
-// car movement control pins
+/*
+** Details : Car movement control pins
+**
+** Connect below output pins  to RF controller's movement pins
+** i.e. connect below pins to the left,righ,forward and reverse to
+** remote's respective buttons(signal triggers)
+*/
+
 const int leftTurnPin = 4; // Output Pin
 const int rightTurnPin = 5; // Output Pin
 const int reverseMovePin = 6; // Output Pin
@@ -18,6 +25,7 @@ void setup() {
   
 Serial.begin(115200); // opens serial port, sets data rate to 9600 bps
 
+// Declare signal pins as OUTPUT pins
 pinMode(leftTurnPin, OUTPUT);
 pinMode(rightTurnPin, OUTPUT);
 pinMode(reverseMovePin, OUTPUT);
@@ -27,18 +35,24 @@ pinMode(forwardMovePin, OUTPUT);
 
 void loop() {
 
+    // read from serial while data is available
     while(Serial.available()){
       data = Serial.read();        
     }
 
-    // convert to int
+    // convert to int 
+    // value sent from keyboard is = 65 + (a combination of 4 bits )
     val = data - 65 ;
 
+    // each bit of represents the button pressed on keyboard
     reverse = val & 1<<0;
     forward = val & 1<<1;
     right   = val & 1<<2;
     left    = val & 1<<3;
 
+    // Write to arduino outpins the signals recieved from keyboard 
+    // My RC controller worked on active low pins, hence I had to invert the signals
+    
     digitalWrite(leftTurnPin, !left);
     digitalWrite(rightTurnPin, !right);
     digitalWrite(reverseMovePin, !reverse);
